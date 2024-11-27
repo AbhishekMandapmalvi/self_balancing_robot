520b55b70c4581f8ee481d243b88085eaaa52ef2
AbhishekMandapmalvi <arm21@uw.edu>
2024-11-27 02:23:15 -0500

    python test code updated

--- src/self_balancing_robot_hardware/scripts/hardware_test.py
+++ src/self_balancing_robot_hardware/scripts/hardware_test.py
@@ -1,75 +1,62 @@
 #!/usr/bin/env python3
 
-import roslibpy
+import rospy
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Int32, Int32MultiArray
 import time
-import sys
-import os
-
-# Add parent directory to path for imports
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 class HardwareTest:
     def __init__(self):
-        # Initialize ROS client
-        self.client = roslibpy.Ros(host='localhost', port=9090)
-        self.client.run()
+        # Initialize ROS node
+        rospy.init_node('hardware_tester')
 
         # Create subscribers
-        self.imu_subscriber = roslibpy.Topic(self.client, '/imu_data', 'sensor_msgs/Imu')
-        self.left_encoder_subscriber = roslibpy.Topic(self.client, '/left_encoder', 'std_msgs/Int32')
-        self.right_encoder_subscriber = roslibpy.Topic(self.client, '/right_encoder', 'std_msgs/Int32')
+        rospy.Subscriber('/imu_data', Imu, self.imu_callback)
+        rospy.Subscriber('/left_encoder', Int32, self.left_encoder_callback)
+        rospy.Subscriber('/right_encoder', Int32, self.right_encoder_callback)
         
         # Create publisher
-        self.motor_publisher = roslibpy.Topic(self.client, '/motor_cmd', 'std_msgs/Int32MultiArray')
-
-        # Subscribe to topics
-        self.imu_subscriber.subscribe(self.imu_callback)
-        self.left_encoder_subscriber.subscribe(self.left_encoder_callback)
-        self.right_encoder_subscriber.subscribe(self.right_encoder_callback)
+        self.motor_publisher = rospy.Publisher('/motor_cmd', Int32MultiArray, queue_size=10)
 
     def imu_callback(self, message):
-        orientation = message['orientation']
-        angular_velocity = message['angular_velocity']
-        linear_acceleration = message['linear_acceleration']
-        
         print("\nIMU Data:")
-        print(f"Orientation (w,x,y,z): {orientation['w']:.2f}, {orientation['x']:.2f}, "
-              f"{orientation['y']:.2f}, {orientation['z']:.2f}")
-        print(f"Angular Velocity (x,y,z): {angular_velocity['x']:.2f}, "
-              f"{angular_velocity['y']:.2f}, {angular_velocity['z']:.2f}")
-        print(f"Linear Acceleration (x,y,z): {linear_acceleration['x']:.2f}, "
-              f"{linear_acceleration['y']:.2f}, {linear_acceleration['z']:.2f}")
+        print(f"Orientation (w,x,y,z): {message.orientation.w:.2f}, {message.orientation.x:.2f}, "
+              f"{message.orientation.y:.2f}, {message.orientation.z:.2f}")
+        print(f"Angular Velocity (x,y,z): {message.angular_velocity.x:.2f}, "
+              f"{message.angular_velocity.y:.2f}, {message.angular_velocity.z:.2f}")
+        print(f"Linear Acceleration (x,y,z): {message.linear_acceleration.x:.2f}, "
+              f"{message.linear_acceleration.y:.2f}, {message.linear_acceleration.z:.2f}")
 
     def left_encoder_callback(self, message):
-        print(f"\nLeft Encoder Count: {message['data']}")
+        print(f"\nLeft Encoder Count: {message.data}")
 
     def right_encoder_callback(self, message):
-        print(f"Right Encoder Count: {message['data']}")
+        print(f"Right Encoder Count: {message.data}")
 
     def run_test(self):
+        rate = rospy.Rate(0.5)  # 0.5 Hz for 2-second intervals
         try:
-            while self.client.is_connected:
+            while not rospy.is_shutdown():
                 # Send test motor commands
-                motor_msg = {'data': [100, -100]}
-                self.motor_publisher.publish(roslibpy.Message(motor_msg))
+                motor_msg = Int32MultiArray(data=[100, -100])
+                self.motor_publisher.publish(motor_msg)
                 print("\nSent motor commands: Left=100, Right=-100")
-                time.sleep(2)
+                rate.sleep()
                 
                 # Stop motors
-                motor_msg = {'data': [0, 0]}
-                self.motor_publisher.publish(roslibpy.Message(motor_msg))
+                motor_msg = Int32MultiArray(data=[0, 0])
+                self.motor_publisher.publish(motor_msg)
                 print("\nSent motor commands: Left=0, Right=0")
-                time.sleep(2)
+                rate.sleep()
 
         except KeyboardInterrupt:
             self.cleanup()
 
     def cleanup(self):
-        self.imu_subscriber.unsubscribe()
-        self.left_encoder_subscriber.unsubscribe()
-        self.right_encoder_subscriber.unsubscribe()
-        self.motor_publisher.unadvertise()
-        self.client.terminate()
+        # Stop motors before shutting down
+        motor_msg = Int32MultiArray(data=[0, 0])
+        self.motor_publisher.publish(motor_msg)
+        rospy.signal_shutdown("User requested shutdown")
 
 if __name__ == '__main__':
     tester = HardwareTest()
