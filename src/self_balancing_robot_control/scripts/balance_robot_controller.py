a617ba80f52a6bbdcd8dedb2d52e82c199f26810
AbhishekMandapmalvi <arm21@uw.edu>
2024-12-17 20:53:04 -0500

    Updated message reception to follow coding stds added udrf for rviz

--- src/self_balancing_robot_control/scripts/balance_robot_controller.py
+++ src/self_balancing_robot_control/scripts/balance_robot_controller.py
@@ -1,37 +1,50 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Float32MultiArray
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 
 class BalanceRobotController:
     def __init__(self):
         rospy.init_node('balance_robot_controller_main', anonymous=True)
-                
-        # Subscriber for sensor data
-        rospy.Subscriber('sensor_data', Float32MultiArray, self.sensor_callback)
+        
+        # Subscriber for odometry data
+        rospy.Subscriber('odom', Odometry, self.odom_callback)
 
-        self.rate = rospy.Rate(20)  # 100Hz
+        # Publisher for cmd_vel (to send commands to Arduino)
+        self.cmd_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
-    def sensor_callback(self, msg):
-        # Process received sensor data
-        accel = msg.data[0:3]    # accelerometer data
-        gyro = msg.data[3:6]     # gyroscope data
-        euler = msg.data[6:9]    # euler angles
-        left_vel = msg.data[9]   # left wheel velocity
-        right_vel = msg.data[10] # right wheel velocity
-        left_dist = msg.data[11] # left wheel distance
-        right_dist = msg.data[12] # right wheel distance
-        us_distance = msg.data[13] - 7.00 # Distance
-        Speed_error = msg.data[14]
-        TurnSpeed_error = msg.data[15]
-        TargetSpeed = msg.data[16]
-        TurnSpeed = msg.data[17]
+        self.rate = rospy.Rate(20)  # 20Hz
+
+    def odom_callback(self, msg):
+        # Process received odometry data
+        position = msg.pose.pose.position
+        orientation = msg.pose.pose.orientation
+        linear_velocity = msg.twist.twist.linear
+        angular_velocity = msg.twist.twist.angular
 
         # Log data (for debugging)
-        rospy.loginfo("w: %.2f, Yaw: %.2f, TargetSpeed: %.2f, TurnSpeed: %.2f, Encoder(left_vel: %.2f, right_vel: %.2f, left_dist: %.2f, right_dist: %.2f, Speed_error: %.2f, TurnSpeed_error: %.2f)", 
-                     gyro[2], euler[2], TargetSpeed, TurnSpeed, left_vel, right_vel, left_dist, right_dist, Speed_error, TurnSpeed_error)
+        rospy.loginfo(
+            "Position: (x: %.2f, y: %.2f), Orientation: (x: %.2f, y: %.2f, z: %.2f, w: %.2f), "
+            "Linear Velocity: (x: %.2f), Angular Velocity: (z: %.2f)",
+            position.x, position.y,
+            orientation.x, orientation.y, orientation.z, orientation.w,
+            linear_velocity.x,
+            angular_velocity.z
+        )
+
+    def send_default_cmd(self):
+        # Create a Twist message with default values (zeros)
+        cmd_msg = Twist()
+        cmd_msg.linear.x = 0.0   # Default speed
+        cmd_msg.angular.z = 0.0  # Default turn angle
+
+        # Publish the message to cmd_vel topic
+        self.cmd_pub.publish(cmd_msg)
 
     def run(self):
         while not rospy.is_shutdown():
+            # Send default command message at regular intervals
+            self.send_default_cmd()
             self.rate.sleep()
 
 if __name__ == '__main__':
